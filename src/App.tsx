1f5eb077c494c3d218c8fde7beb4c83a3127ee55
Dávid Kecskeméti <dxdy421@gmail.com>
2021-02-10 19:28:56 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Engine } from "json-rules-engine";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const engine = new Engine();
+engine.addRule({
+  conditions: {
+    any: [
+      {
+        all: [
+          {
+            fact: "cost",
+            operator: "lessThanInclusive",
+            value: 400,
+          },
+          {
+            fact: "cost",
+            operator: "greaterThan",
+            value: 0,
+          },
+        ],
+      },
+    ],
+  },
+  event: {
+    type: "canTrade",
+    params: {
+      message: "Trade can be entered",
+    },
+  },
+});
+
+interface StateProps {
+  shares: number;
+  unitPrice: number;
 }
+
+interface ResultProps {
+  results: string;
+}
+
+interface EngineParam {
+  message: string;
+}
+interface EventProps {
+  params: EngineParam;
+}
+const App: React.FC = () => {
+  const [state, setState] = useState<StateProps>({ shares: 0, unitPrice: 0 });
+  const [results, setResults] = React.useState<ResultProps>();
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const newState = {
+      ...state,
+      [name]: +value,
+    };
+    setState(newState);
+
+    engine
+      .run({
+        cost: newState.shares * newState.unitPrice,
+      })
+      .then((engineResult) => {
+        const res = engineResult.events.map((e) => e.params?.message!);
+        setResults(res[0]);
+      });
+  };
+
+  return (
+    <form>
+      <div>
+        <label>Balance: 400</label>
+      </div>
+      <div>
+        <label>
+          Shares
+          <input
+            type="text"
+            name="shares"
+            value={state.shares}
+            onChange={onChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Unit Price
+          <input
+            type="text"
+            name="unitPrice"
+            value={state.unitPrice}
+            onChange={onChange}
+          />
+        </label>
+      </div>
+      <div>Result: {results}</div>
+      <div>
+        Next Steps: add a rule that checks Shares and Unit Price are bigger than
+        zero
+      </div>
+    </form>
+  );
+};
 
 export default App;
